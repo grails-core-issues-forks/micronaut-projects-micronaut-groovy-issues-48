eb14573c53d8cf582278bcfefd79c00f983d5ad5
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-05 11:35:29 +0200

    remove workaround

--- src/main/groovy/example/micronaut/Application.groovy
+++ src/main/groovy/example/micronaut/Application.groovy
@@ -2,17 +2,10 @@ package example.micronaut
 
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
-import org.grails.datastore.mapping.config.ConfigurationBuilder
-
-import java.lang.reflect.Field
 
 @CompileStatic
 class Application {
     static void main(String[] args) {
-        Field field = ConfigurationBuilder.class.getDeclaredField('IGNORE_METHODS')
-        field.setAccessible(true)
-        ((Set) field.get(null)).add('autoEncryptionSettings')
-
         Micronaut.run(Application)
     }
 }
